720f53dba7f08bc2f0090982e1ae89d045deea56
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-08-14 00:39:18 +0530

    Fix facebook plugin issue

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://nybles.herokuapp.com',
+        url: 'http://nybles.tech',
         mail: {},
 
         database: {  
